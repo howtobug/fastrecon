8eb3d4c7fa169e474ea6a9d3ac1413ba2871f2fc
Américo Júnior <62674418+howtobug@users.noreply.github.com>
2020-05-18 01:01:10 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_file
 from flask_sqlalchemy import SQLAlchemy
 from os import system
 
