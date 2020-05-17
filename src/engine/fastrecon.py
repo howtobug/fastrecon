ec6a1aa0cec7e1f3311ba291569e644834067c2a
Américo Júnior <62674418+howtobug@users.noreply.github.com>
2020-05-17 23:56:08 +0200

    Version 1.0.4

--- src/engine/fastrecon.py
+++ src/engine/fastrecon.py
@@ -8,42 +8,42 @@ except:
         print("Syntax Error")
         exit()
 
-system("mkdir engine/results/{}".format(domain))
+system("mkdir /results/{}".format(domain))
 
 
 def dig(domain):
-        system("dig {} > engine/results/{}/dig.txt".format(domain, domain))
+        system("dig {} > results/{}/dig.txt".format(domain, domain))
 
 def host(domain):
-        system("host {} > engine/results/{}/host.txt".format(domain, domain))
+        system("host {} > results/{}/host.txt".format(domain, domain))
 
 
 def assetfinder(domain):
-        system("assetfinder --subs-only {} > engine/results/{}/subdomain-enumeration.txt".format(domain, domain))
+        system("assetfinder --subs-only {} > results/{}/subdomain-enumeration.txt".format(domain, domain))
 
 def dnscan():
-        system("python3 engine/tools/dnscan/dnscan.py -d {} -w engine/tools/dnscan/subdomains-1000.txt > engine/results/{}/subdomain-bruteforce-demo.txt".format(domain, domain))
-        system("cat engine/results/{}/subdomain-bruteforce-demo.txt | grep '{}' | cut -d '-' -f2 > engine/results/{}/subdomain-bruteforce.txt ".format(domain, domain, domain))
+        system("python3 tools/dnscan/dnscan.py -d {} -w tools/dnscan/subdomains-1000.txt > results/{}/subdomain-bruteforce-demo.txt".format(domain, domain))
+        system("cat results/{}/subdomain-bruteforce-demo.txt | grep '{}' | cut -d '-' -f2 > results/{}/subdomain-bruteforce.txt ".format(domain, domain, domain))
 
 def getallurls():
-        system("cat engine/results/{}/all-subdomains.txt | gau > engine/results/{}/spider.txt".format(domain, domain))
+        system("cat results/{}/all-subdomains.txt | gau > results/{}/spider.txt".format(domain, domain))
 
 def nmap():
-        system("nmap -iL engine/results/{}/ips.txt > engine/results/{}/nmapOutput.txt".format(domain, domain))
+        system("nmap -iL results/{}/ips.txt > results/{}/nmapOutput.txt".format(domain, domain))
 
 def waybackurls():
-        system("touch engine/results/{}/all-subdomains.txt".format(domain))
-        system("cat engine/results/{}/subdomain-enumeration.txt >> engine/results/{}/all-subdomains.txt".format(domain, domain))
-        system("cat engine/results/{}/subdomain-bruteforce.txt >> engine/results/{}/all-subdomains.txt".format(domain, domain))
-        system("cat engine/results/{}/all-subdomains.txt | waybackurls > engine/results/{}/waybackurls.txt".format(domain, domain))
+        system("touch results/{}/all-subdomains.txt".format(domain))
+        system("cat results/{}/subdomain-enumeration.txt >> results/{}/all-subdomains.txt".format(domain, domain))
+        system("cat results/{}/subdomain-bruteforce.txt >> results/{}/all-subdomains.txt".format(domain, domain))
+        system("cat results/{}/all-subdomains.txt | waybackurls > results/{}/waybackurls.txt".format(domain, domain))
 
 def aquatone():
         #system("mkdir engine/results/{}/aquatone".format(domain))
-        system("cat engine/results/{}/all-subdomains.txt | aquatone -out engine/results/{}/aquatone".format(domain, domain))
+        system("cat results/{}/all-subdomains.txt | aquatone -out results/{}/aquatone".format(domain, domain))
 
 def getIps():
-        system("cp engine/results/{}/all-subdomains.txt engine/tools/getIps".format(domain))
-        system("python3 engine/tools/getIps/getIps.py {} > engine/results/{}/ips.txt".format(domain, domain))
+        system("cp results/{}/all-subdomains.txt tools/getIps".format(domain))
+        system("python3 tools/getIps/getIps.py {} > results/{}/ips.txt".format(domain, domain))
 
 
 dig(domain)
